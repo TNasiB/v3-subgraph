4d5e268370b6a42e6c3e178a567f310e37e7e721
Тимерьян Насибуллин <-github-email@example.com>
2024-08-28 13:21:23 +0500

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const FACTORY_ADDRESS = '0x3E2CE267250382b0cd1Bf22884E9236B85062911'
 
 export const ZERO_BI = BigInt.fromI32(0)
 export const ONE_BI = BigInt.fromI32(1)
